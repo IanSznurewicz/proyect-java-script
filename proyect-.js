70cd50b8ab26ab29e43689de6e5ee871c5df1366
Ian Sznurewicz <ianfriendss@gmail.com>
2023-01-15 23:47:45 -0300

    fix-register-i-session

--- proyect-.js
+++ proyect-.js
@@ -1,131 +1,202 @@
-    //Linea 1------------------------------------------------------------------------------------------------------------------------------------------------=-------------
 
-                            //Nombre y Apellido
-    let nombreyapellido = prompt("Ingresar Nombre y Apellido (no debe contener numeros)")
-    if (nombreyapellido !== "" ){
-        alert("El Nombre y Apellido fueron ingresados correctamente")
+    let repeate9 = false
+    let repeate8 = false
+    let repeate7 = false
+    let repeate6 = false
+    let repeate5 = false
+    let repeate4 = false
+    let repeate3 = false                     
+    let repeate2 = false 
+    let repeatenya = false
+                                //Sin Funcionalidad de momento
+                                
+   while (!repeate3){
+    break
+    let srIs = prompt("1) Si desea registrarse escriba: Register                                                     2) Si desea iniciar session escriba: Inicio Session")
+    if ((srIs !== "") && (srIs == "Register") || (srIs == "Inicio Session")){
+        repeate3 = true 
+        console.log (`${srIs}`)
     }else{
-        alert("ERROR: Ingresar nombre y apellido validos.")
+        alert("ERROR: Ingresar una de las opciones otorgadas")
+        continue
     }
-                            //Nombre y Apellido
-   
-
-                            //Contraseña
-    let contrasenia = prompt("Ingresar Contraseña") 
-    if (contrasenia !== "") {
-        alert("La Contraseña fue ingresada correctamente")
-    }else{
-        alert("ERROR: Ingresar contraseña valida.")        
-    } 
-                            //Contraseña
-
-    console.log(`El nombre y apellido es ${nombreyapellido} de la session la cual tiene una contraseña la cual es ${contrasenia}`)
-        alert(`Su registro de session fue realizado correctamente. Su Nombre y Apellido ingresados fueron ${nombreyapellido}. Su contraseña ingresada fue ${contrasenia}`)
-
-
-    let resetSession = prompt("Si desea cambiar su registro de session totalmente escriba: SI. Si desea dejarlo como esta actualmente escriba: NO. Si desea modificar unicamente el nombre y apellido escriba: Nombre y Apellido. Si desea modificar unicamente la contraseña escriba: Contraseña.")
-        console.log(resetSession)
-    if((resetSession !== "") && ((resetSession == "SI") || (resetSession == "si") || (resetSession == "NO") || (resetSession == "no") || (resetSession == "Nombre y Apellido") || (resetSession == "Contraseña"))){
-            console.log("Gracias por registrarse en PizzSpeed")
-    }else{
-        alert("ERROR: Invalido")
-    }
-
-        //-------------------------------------------------------------- RESET- Opciones Individuales---------------------------------------------------------------
-
-                            //Nombre y Apellido
-    if(resetSession == "Nombre y Apellido"){
-        let nombreyApellido = prompt("Ingresar Nombre y Apellido")
-            alert("El Nombre y Apellido fueron modificados correctamente")
-
-            console.log(`Nombre y Apellido modificado a = ${nombreyApellido} `)
-
-            alert(`Su registro de session fue modificado correctamente. Su Nombre y Apellido actuales son ${nombreyApellido}. Su contraseña no fue modificada por lo que es la siguiente: ${contrasenia}`)
-
-            alert( nombreyApellido + " gracias por registrarte en PizzSpeed")
-                console.log("Gracias por registrarse en PizzSpeed")
-    }
-
-                    //Nombre y Apellido
-
-                    //Contraseña
-    if(resetSession == "Contraseña"){
-        let contraSenia = prompt("Ingresar Contraseña")
-            alert("La Contraseña fue modificada correctamente")
-
-        console.log(`Contraseña modificada a = ${contraSenia}`)
-
-            alert(`Su registro de session fue modificado correctamente. Su Nombre y Apellido no fueron modificados, por lo que son los siguientes: ${nombreyapellido}. Su contraseña actual es ${contraSenia}`)
-
-            alert( nombreyapellido + " gracias por registrarte en PizzSpeed")
-                console.log("Gracias por registrarse en PizzSpeed")
-    }
-                    //Contraseña
-        //-------------------------------------------------------------- RESET- Opciones Individuales---------------------------------------------------------------
+}                                           
+                                //Sin funcionalidad de momento
 
 
 
+                                //Nombre y Apellido   
+    while (!repeatenya){
+        let nombreyapellido = prompt("Ingresar Nombre y Apellido (no debe contener numeros)")
+        if (nombreyapellido !== ""){
+            alert("El Nombre y Apellido fueron ingresados correctamente")
+            repeatenya = true
+            console.log(`Nombre y Apellido: ${nombreyapellido}, queda registrado`)
+        }else{
+            alert("ERROR: Ingresar nombre y apellido validos")
+            console.log(`El nombre y Apellido adjunto: ${nombreyapellido} son invalidos`)
+            continue
+        }
+                                //Nombre y Apellido
 
-        //--------------------------------------------------------------Opciones Generales--------------------------------------------------------------------------
 
-                        //Nombre y Apellido
-    if((resetSession == "SI") || (resetSession == "si")) {
+                                    //Contraseña
+        while (!repeate2){
+            let contrasenia = prompt("Ingresar Contraseña") 
+            if (contrasenia !== ""){
+                alert("La Contraseña fue ingresada correctamente")
+                repeate2 = true
+                console.log(`Contraseña: ${contrasenia}, queda registrada`) 
+            }else{
+                alert("ERROR: Ingresar contraseña valida")     
+                console.log(`Contraseña adjunta: ${contrasenia} es invalida`)
+                continue
+            }
+                                    //Contraseña
+        alert(`REGISTRO de session realizado correctamente: 
 
-        let nombreyapellido2 = prompt("Ingresar Nombre y Apellido")
-        alert("El Nombre y Apellido fueron modificados correctamente")
-    
-                        //Nombre y Apellido
-
-                        //Contraseña
-    let contrasenia2 = prompt("Ingresar Contraseña")
-
-        alert("La Contraseña fue modificada correctamente")
-
-        console.log(`Nombre y Apellido modificados a = ${nombreyapellido2}. Contraseña modificada a = ${contrasenia2}`)
-
-        alert( nombreyapellido2 + " gracias por registrarte en PizzSpeed")
-        console.log("Gracias por registrarse en PizzSpeed")
-    }
-                        //Contraseña
- 
-
-    if((resetSession == "NO") || (resetSession == "no")){
-        alert( nombreyapellido + " gracias por registrarte en PizzSpeed")
-            console.log("Gracias por registrarse en PizzSpeed")
+        1)Nombre y Apellido ingresados: ${nombreyapellido}. 
+        2)Contraseña ingresada: ${contrasenia}.`)
+        console.log(`Nombre y Apellido ingresados: ${nombreyapellido}. Contraseña ingresada: ${contrasenia}; de la session actual registrada.`)
             
-        let inicioSession = prompt("Nombre y Apellido")
-        if((inicioSession !== "") && (inicioSession == `${nombreyapellido}`) && (resetSession == "NO") || (resetSession == "no")){
-            console.log(`${inicioSession}`)
+
+
+
+        let resetSession = prompt(`1) Si desea cambiar su registro de session totalmente escriba: SI
+2) Si desea dejarlo como esta actualmente escriba: NO
+3) Si desea modificar unicamente el nombre y apellido escriba: Nombre y Apellido
+4) Si desea modificar unicamente la contraseña escriba: Contraseña`)
+
+            console.log(resetSession)
+        if((resetSession !== "") && ((resetSession == "SI") || (resetSession == "si") || (resetSession == "NO") || (resetSession == "no") || (resetSession == "Nombre y Apellido") || (resetSession == "Contraseña"))){
+            console.log(`Registro COMPLETADO: Bienvenido a PizzSpeed ${nombreyapellido}`)
         }else{
-            alert("ERROR: Nombre y Apellido incorrectos")
+            alert("ERROR: Invalido")
         }
-    
-        let iniciosession = prompt("Contraseña")
-        if((iniciosession !== "") && (iniciosession == `${contrasenia}`)){
-            console.log(`${iniciosession}`)
-    
-            alert("Inicio de session completado correctamente")
-    
-            console.log(`INICIO DE SESSION CORRECTO: Nombre y Apellido: ${nombreyapellido}. Contraseña: ${contrasenia}`)
-        }else{
-            alert("ERROR: Contraseña incorrecta")
-            alert("Inicio de session completado incorrectamente")
-    
-            console.log(`INICIO DE SESSION INCORRECTO: Nombre y Apellido: ${inicioSession}. Contraseña: ${iniciosession}`)
+    //Inicio de Session------------------------------------------------------------------------------------------------------------------------------------=------------------
+    if((resetSession == "NO") || (resetSession == "no")){
+        alert("REGISTRO COMPLETADO: Bienvenido a PizzSpeed " + nombreyapellido)
+        alert("                                         INICIO DE SESSION")
+        console.log("Registro COMPLETADO: Bienvenido a PizzSpeed")
+
+
+        while ((!repeate4)){
+            let inicioSession = prompt("Nombre y Apellido")
+            if(inicioSession == nombreyapellido){
+                repeate4 = true
+                console.log(inicioSession)
+            }else{
+                alert("ERROR: Nombre y Apellido incorrectos")
+                continue
+            }
+        
+
+        while ((!repeate5)){
+            let iniciosession = prompt("Contraseña")
+            if(iniciosession == contrasenia){
+                repeate5 = true
+                console.log(iniciosession)
+            }else{
+                alert("ERROR: Contraseña incorrecta")
+                continue   
+                }if ((iniciosession !== "") !="ESC"){
+                    alert("Inicio de session completado correctamente")
+                    console.log(`INICIO DE SESSION CORRECTO: Nombre y Apellido: ${nombreyapellido}. Contraseña: ${contrasenia}`)
+                }else{
+                    alert("Inicio de session completado incorrectamente")
+                    console.log(`INICIO DE SESSION INCORRECTO: Nombre y Apellido: ${inicioSession}. Contraseña: ${iniciosession}`)
+                    continue
+                }
+            }
         }
     }
+    //Inicio de Session------------------------------------------------------------------------------------------------------------------------------------=------------------
+
+            //--------------------------------------------------------------Opciones Generales--------------------------------------------------------------------------
+    if((resetSession == "SI") || (resetSession == "si")) {
+        while (!repeate6){
+                                //Nombre y Apellido
+            let nombreyApellido = prompt("Ingresar Nombre y Apellido")
+            if(nombreyApellido !== ""){
+                repeate6 = true
+                alert("AVISO: El Nombre y Apellido fueron modificados correctamente")
+            }else{
+                alert("ERROR: Ingresar nombre y apellido validos")
+                console.log(`El nombre y Apellido adjunto: ${nombreyApellido} son invalidos`)
+                continue
+            }
+        
+                                //Nombre y Apellido
+
+        while (!repeate7){
+                                //Contraseña
+            let contrAsenia = prompt("Ingresar Contraseña")
+            if(contrAsenia !== ""){
+                repeate7 = true
+                alert("AVISO: La Contraseña fue modificada correctamente")
+            }else{
+                alert("ERROR: Ingresar contraseña valida")     
+                console.log(`Contraseña adjunta: ${contrAsenia} es invalida`)
+                continue
+            }if (resetSession !== ""){
+                repeate7 = true
+                alert(`INFORME: Su registro de session fue modificado correctamente
+
+    1) Su Nombre y Apellido fueron modificados a: ${nombreyApellido}
+    2) Su contraseña fue modificada a: ${contrAsenia}`)
+                alert("Registro COMPLETADO: Bienvenido a PizzSpeed " + nombreyApellido )
+                console.log(`Nombre y Apellido modificados a = ${nombreyApellido}. Contraseña modificada a = ${contrAsenia}`)
+                console.log("Registro COMPLETADO: Bienvenido a PizzSpeed")
+            }
+                                //Contraseña
+                            }
+                        }
+                    }  
         //--------------------------------------------------------------Opciones Generales--------------------------------------------------------------------------
-
-//Inicio de Session------------------------------------------------------------------------------------------------------------------------------------=------------------
-//Inicio de Session------------------------------------------------------------------------------------------------------------------------------------------=------------
+                       
 
 
+        //-------------------------------------------------------------- RESET- Opciones Individuales---------------------------------------------------------------
+   if (resetSession == "Nombre y Apellido") {
+    while(!repeate8){
+                            //Nombre y Apellido
+        let nombreyApellido = prompt("Ingresar Nombre y Apellido")
+        if((nombreyApellido !== "")  && (resetSession == "Nombre y Apellido")){
+            repeate8 = true
+                alert("AVISO: El Nombre y Apellido fueron modificados correctamente")
+                console.log(`Nombre y Apellido modificado a = ${nombreyApellido} `)
+                alert(`INFORME: Su registro de session fue modificado correctamente. Su Nombre y Apellido actuales son ${nombreyApellido}. Su contraseña no fue modificada por lo que es la siguiente: ${contrasenia}`)
 
+                alert("Registro COMPLETADO: Bienvenido a PizzSpeed " + nombreyApellido )
+                console.log("Registro COMPLETADO: Bienvenido a PizzSpeed")
+            }else{
+                alert("ERROR: Ingresar nombre y apellido validos")
+                console.log(`El nombre y Apellido adjunto: ${nombreyApellido} son invalidos`)
+            }
+                            //Nombre y Apellido
+        }
+    }
 
- 
+    if (resetSession == "Contraseña"){
+    while(!repeate9){
+                            //Contraseña
+        let contrAsenia = prompt("Ingresar Contraseña")
+        if((contrAsenia !== "") && (resetSession == "Contraseña")){
+            repeate9 = true
+                alert("AVISO: La Contraseña fue modificada correctamente")
+                console.log(`Contraseña modificada a = ${contrAsenia}`)
+                alert(`INFORME: Su registro de session fue modificado correctamente. Su Nombre y Apellido no fueron modificados, por lo que son los siguientes: ${nombreyapellido}. Su contraseña actual es ${contrAsenia}`)
 
-
-
-    //Linea 1------------------------------------------------------------------------------------------------------------------------------------------------=-------------
-
-
+                alert("Registro COMPLETADO: Bienvenido a PizzSpeed " + nombreyapellido)
+                console.log("Registro COMPLETADO: Bienvenido a PizzSpeed")
+            }else{
+                alert("ERROR: Ingresar contraseña valida")     
+                console.log(`Contraseña adjunta: ${contrAsenia} es invalida`)
+            }
+                            //Contraseña
+        }
+    }
+            //-------------------------------------------------------------- RESET- Opciones Individuales---------------------------------------------------------------
+          
+                    }   
+                }
